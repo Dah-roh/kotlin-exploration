77ec5cc78e88839ed46b8efbf68645ca326ac0ee
dizsart <darogadibia@gmail.com>
2020-03-15 02:09:11 +0100

    adding the getter and setter functions to the file structure in the kotlin project

--- src/com/learningkotlin/kolin/classes/Customer.kt
+++ src/com/learningkotlin/kolin/classes/Customer.kt
@@ -10,6 +10,8 @@ constructor(name: String, address: String, age: Int) {
     this.address = address
     this.age = age
 }
+
+
     constructor(name: String, age: Int): this(name, "", age) {
         println("secondary constructor")
     }
@@ -17,13 +19,27 @@ constructor(name: String, address: String, age: Int) {
     init {
         println("init block")
     }
+
+    var approved: Boolean = false
+        set(value) {
+            if (age<=100) {
+                field = value
+            }
+            else {
+                println("You can't approve a customer under 100 years old")
+            }
+        }
+
+    val nextAge
+    get() = age + 1
 }
 
 fun main () {
     val customer = Customer()
 customer.age = 23
     val secondCustomer = Customer("Gadibia", "18 ijeoma odika street", 78)
-    println("${customer.age} and ${secondCustomer.age}")
+    secondCustomer.approved = true
+    println("${customer.age} is ${customer.approved} and ${secondCustomer.age} ${secondCustomer.approved}")
     val secondaryConstructor = Customer("Oghentevwodaro", 80)
-    secondaryConstructor.age
+    println("${secondaryConstructor.age}")
 }
